6be1da278247b326d731e9ab609659830a159bb4
Javier Garcia <jgp@sens2web.es>
2020-02-26 23:25:22 +0100

    temario from CD

---
+++ cmake-build-debug/CMakeFiles/lenguajes_programacion.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/eleuteron/Documents/uned/2s/lenguajes-programacion/Tema_1/CalculoSuma.cpp" "/home/eleuteron/Documents/uned/2s/lenguajes-programacion/cmake-build-debug/CMakeFiles/lenguajes_programacion.dir/Tema_1/CalculoSuma.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Tema_1"
+  "../Tema_10"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/lenguajes_programacion.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lenguajes_programacion.dir/Tema_1/CalculoSuma.cpp.o"
+  "lenguajes_programacion"
+  "lenguajes_programacion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lenguajes_programacion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
